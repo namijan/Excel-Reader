94d1a6b32b92229af72dded59f5ed05e7ca89dbb
namijan <namijan@gmail.com>
2020-06-11 00:06:41 +0530

    Add files via upload

---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 64);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "*.xls|*.xlsx";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "Select an Excel file";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(893, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(199, 64);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Read";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1115, 200);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(1147, 288);
+            this.MinimumSize = new System.Drawing.Size(1147, 288);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Excel Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            label1.Text = openFileDialog1.FileName;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "")
+                MessageBox.Show("Please select a file first", "Error");
+            else
+            {
+                try
+                {
+                    //create a instance for the Excel object  
+                    Excel.Application oExcel = new Excel.Application();
+                    //specify the file name where its actually exist  
+                    string filepath = label1.Text;
+                    //pass that to workbook object  
+
+                    MessageBox.Show("I will now attempt to read the file after you hit ok");
+                    Excel.Workbook WB = oExcel.Workbooks.Open(filepath);
+
+                    MessageBox.Show("Looks like I am able to read the file. I will proceed to give you some relevant details now");
+
+                    //  get the workbookname  
+                    string ExcelWorkbookname = WB.Name;
+
+                    //  get the worksheet count  
+                    int worksheetcount = WB.Worksheets.Count;
+
+                    Excel.Worksheet wks = (Excel.Worksheet)WB.Worksheets[1];
+
+                    //  get the firstworksheetname  
+                    string firstworksheetname = wks.Name;
+
+                    //statement get the first cell value  
+                    var firstcellvalue = ((Excel.Range)wks.Cells[1, 1]).Value;
+
+
+                    MessageBox.Show(
+
+                        "Workbook name = " + ExcelWorkbookname + '\n' +
+                        "Count of worksheets = " + worksheetcount + '\n' +
+                        "Name of the first worksheet = " + firstworksheetname + '\n' +
+                        "First cell value = " + firstcellvalue, "READ WAS SUCCESSFUL");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("I caught an exception. Hit ok to get details");
+                    string error = ex.Message;
+                    MessageBox.Show(error, "READ WAS UNSUCCESSFUL");
+                }
+            }
+        }
+    }
+}
